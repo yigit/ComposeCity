065c051a11e921813b890293a46f2dd585078ea7
Yigit Boyar <yboyar@gmail.com>
2021-08-08 10:49:05 -0700

    remove jb copyright as it seems like was generated by the template

--- src/main/kotlin/GameUI.kt
+++ src/main/kotlin/GameUI.kt
@@ -1,4 +1,3 @@
-// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 import androidx.compose.animation.animateColorAsState
 import androidx.compose.desktop.DesktopMaterialTheme
 import androidx.compose.foundation.Image
